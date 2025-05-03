762c1e35f54ac175123be31e3a24b37317397eac
v3ska <vasiltreska@gmail.com>
2025-05-04 00:28:21 +0300

    Add files via upload

---
+++ AML practise.sql
@@ -0,0 +1,5 @@
+INSERT INTO Accounts (AccountID, CustomerID, AccountType, Balance, OpenDate) VALUES
+(1, 1, 'Savings', 5000.00, '2020-01-01'),
+(2, 2, 'Checking', 3000.00, '2019-05-20'),
+(3, 3, 'Savings', 7000.00, '2021-07-30'),
+(4, 4, 'Checking', 10000.00, '2018-12-10');
